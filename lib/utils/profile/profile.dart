85506d672efbb2b1c0284abcab2a7f30fdfe1ee9
Sagar Monga <sagar.monga3097@gmail.com>
2021-07-14 14:19:01 +0530

    Profile Screen Created

--- lib/utils/profile/profile.dart
+++ lib/utils/profile/profile.dart
@@ -1,12 +1,279 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({ Key? key }) : super(key: key);
+class Profile extends StatefulWidget {
+  Profile({Key? key}) : super(key: key);
 
   @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final List<String> images = [
+    'https://images.unsplash.com/photo-1626123080782-10b336a160b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1366&q=80',
+    'https://images.unsplash.com/photo-1626128380912-536d8fdbe829?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1625913952228-8d3fcc4ff5ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80',
+    'https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1626009374570-11e2952306b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+    'https://images.unsplash.com/photo-1626123080782-10b336a160b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1366&q=80',
+    'https://images.unsplash.com/photo-1626049789315-2d5f1b656454?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=357&q=80',
+    'https://images.unsplash.com/photo-1626080308326-a6131ac180db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1626099964608-6c30072f0812?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+    'https://images.unsplash.com/photo-1625913952228-8d3fcc4ff5ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80',
+    'https://images.unsplash.com/photo-1626128380912-536d8fdbe829?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1626009374570-11e2952306b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+    'https://images.unsplash.com/photo-1626089697398-506b73fccd0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+    'https://images.unsplash.com/photo-1626049789315-2d5f1b656454?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=357&q=80',
+    'https://images.unsplash.com/photo-1626080308326-a6131ac180db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1626099964608-6c30072f0812?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+  ];
+  Size _size = Size(0, 0);
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Profile'),),
+    _size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).canvasColor,
+          leading: Icon(
+            Icons.lock_outline,
+            color: Colors.white,
+          ),
+          title: Row(
+            children: [
+              Text(
+                'alpha.3628__',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Icon(
+                Icons.keyboard_arrow_down,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add_box_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu),
+            ),
+          ],
+        ),
+        body: DefaultTabController(
+          length: 2,
+          child: ListView(
+            children: [
+              //Profile part
+              _profileSummary(),
+              //Story Highlights in horizontal SingleChildScrollView
+
+              //Tab Bar for showing image grid or tagged images
+              _tabbedView(),
+
+              // GridView to display images
+              _imagesGridView(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  GridView _imagesGridView() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 3,
+        crossAxisSpacing: 3,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          child: Image.network(
+            images[index],
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+      itemCount: images.length,
+    );
+  }
+
+  TabBar _tabbedView() {
+    return TabBar(
+      tabs: [
+        Tab(
+          icon: Icon(
+            Icons.grid_on_outlined,
+            color: Colors.white,
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.person_pin_sharp,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Padding _profileSummary() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://img.glyphs.co/img?src=aHR0cHM6Ly9zMy5tZWRpYWxvb3QuY29tL3Jlc291cmNlcy9WZWN0b3ItUG9rZW1vbi1GYWNlcy1QcmV2aWV3LTEuanBn&q=90&enlarge=true&h=1036&w=1600'),
+                ),
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  Text(
+                    '45',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    'Posts',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: _size.width * 0.10,
+              ),
+              Column(
+                children: [
+                  Text(
+                    '333',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+              SizedBox(width: _size.width * 0.10),
+              Column(
+                children: [
+                  Text(
+                    '127',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    'Following',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+              SizedBox(width: _size.width * 0.05),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+            'alpha.3628__',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            'Press the follow button now that you\'re here 🤷🏻‍♂️',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 7,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.white,
+                      )),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                flex: 1,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Center(
+                    child: Icon(
+                      Icons.keyboard_arrow_down_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    side: BorderSide(
+                      width: 1,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
